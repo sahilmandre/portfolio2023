0bd68fabb0dddaa285c69b24cae97c663e6863d8
Sahilmandre <sahilmandre@gmail.com>
2023-01-23 21:21:04 +0530

    projects completed

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 function Projects({}: Props) {
+  const projects = [1, 2, 3, 4, 5];
+
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
       className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row 
     max-w-full justify-evenly mx-auto items-center z-0"
     >
@@ -12,8 +18,42 @@ function Projects({}: Props) {
         Projects
       </h3>
 
-      <div className="w-full absolute top-[30%] bg-[#f7ab0a]/10 left-0 h-[50rem] -skew-y-12"></div>
-    </div>
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project, index) => (
+          <div
+            className="w-screen flex-shrink-0 snap-center flex justify-center items-center  flex-col space-y-5 
+          p-20 md:p-44 h-screen "
+          >
+            <motion.img
+              initial={{ y: -300, opacity: 0 }}
+              transition={{ duration: 1.2 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              src="https://portfolio-sahilmandre.vercel.app/assets/images/projectImages/hulu.webp"
+              alt=""
+            />
+
+            <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+              <h4 className="text-4xl font-semibold text-center">
+                <span className="underline decoration-[#f7ab04]/50">
+                  {" "}
+                  Project {index + 1} of {projects.length}:
+                </span>{" "}
+                Hulu Clone
+              </h4>
+              <p className="text-lg text-center md:text-left ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+                aliquam, quidem ullam, porro blanditiis sint eius qui aut
+                dignissimos, sapiente quis sit optio aliquid pariatur ratione
+                deserunt facere ex ad?
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="w-full absolute top-[30%] bg-[#f7ab0a]/10 left-0 h-[31rem] -skew-y-12"></div>
+    </motion.div>
   );
 }
 
